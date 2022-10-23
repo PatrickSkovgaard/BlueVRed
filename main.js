194bb28eeb7387868502206990a5a5792cd7aadd
Patrick Sommer Skovgaard <skovskiver@gmail.com>
2022-10-23 23:57:29 +0200

    Added navbar links, plus some extra css and js

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-//to be done...
+let blue = 0
+let red = 0
+
+highlightSide()
+
+function highlightSide(){
+
+    document.getElementById("left")
+    .addEventListener("dblclick", (e)=>{
+        if(blue % 2 === 0){
+            document.getElementById("left").style.backgroundColor = "rgb(0,0,150)";
+        }
+        else{
+            document.getElementById("left").style.backgroundColor = "rgb(0,0,100)";
+        }
+
+        blue++
+})
+
+
+    document.getElementById("right")
+    .addEventListener("dblclick", (e)=>{
+        if(red % 2 === 0){
+            document.getElementById("right").style.backgroundColor = "rgb(150,0,0)";
+        }
+        else{
+            document.getElementById("right").style.backgroundColor = "rgb(100,0,0)";
+        }
+
+        red++
+})
+}
+
+
